0ef2f88950b778ff3c2653d76c8eb91053ed4b28
claudiomss <claudiocld95@gmail.com>
2023-11-13 02:14:08 -0300

    fix: bug deploy 1.1

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 }
+
+module.exports = nextConfig
